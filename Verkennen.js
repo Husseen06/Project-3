8e8cbb6b4f4bc6221066194107ad553380216a98
Husseen <147936667+Husseen06@users.noreply.github.com>
2024-04-15 00:16:49 +0200

    Update Verkennen.js

--- Verkennen.js
+++ Verkennen.js
@@ -1,38 +1,35 @@
-let zoeken = [
-    'Husseen Aden',
-    'Ziad Toumi',
-    'Shaakir Omar',
-  ];
+function followAccount(username) {
+  alert('Je volgt nu ' + username);
   
-  const searchInput = document.getElementById('search');
-  const searchWrapper = document.querySelector('.wrapper');
-  const resultsWrapper = document.querySelector('.results');
-  
-  searchInput.addEventListener('keyup', () => {
-    let results = [];
-    let input = searchInput.value;
-    if (input.length) {
-      results = zoeken.filter((item) => {
-        return item.toLowerCase().includes(input.toLowerCase());
-      });
-    }
-    Resultaten(results);
-  });
-  
-  function Resultaten(results) {
-    if (!results.length) { 
-      return searchWrapper.classList.remove('show');
-    }
-  
-    const content = results
-      .map((item) => {
-        return `<li><a href= "overonsHusseen.html">${"Husseen Aden"}</a></li>,
-    <li><a href= "overonsZiad.html">${"Ziad Toumi"}</a></li>,
-    <li><a href= "overonsShaakir.html">${"Shaakir Omar"}</a></li>`; 
-            
-    })
+}
 
-  
-    searchWrapper.classList.add('show');
-    resultsWrapper.innerHTML = `<a href= "#"><ul>${content}</ul></a>`;
-  }
+let zoeken = [
+'Husseen Aden',
+'Ziad Toumi',
+'Shaakir Omar',
+];
+
+const searchInput = document.getElementById('search');
+const searchWrapper = document.querySelector('.wrapper');
+const resultsWrapper = document.querySelector('.results');
+const searchResults = document.getElementById('search-results');
+
+searchInput.addEventListener('keyup', () => {
+let input = searchInput.value.toLowerCase();
+let result = zoeken.find(item => item.toLowerCase().includes(input));
+if (result) {
+ searchWrapper.classList.add('show');
+ if (result === 'Husseen Aden') {
+   searchResults.innerHTML = `<li><a href="overonsHusseen.html">${result}</a></li>`;
+ } else if (result === 'Shaakir Omar') {
+   searchResults.innerHTML = `<li><a href="OveronsShaakir.html">${result}</a></li>`;
+ } else if (result === 'Ziad Toumi') {
+   searchResults.innerHTML = `<li><a href="overonsZiad.html">${result}</a></li>`;
+ } else {
+   searchResults.innerHTML = `<li><a href="${result.toLowerCase().replace(' ', '')}.html">${result}</a></li>`;
+ }
+} else {
+ searchWrapper.classList.remove('show');
+ searchResults.innerHTML = '';
+}
+});
